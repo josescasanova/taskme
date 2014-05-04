e9309936a1b02d634a10fa6fdf53b990d7bf9af2
Jose Casanova <josescasanova@gmail.com>
2014-05-04 01:53:28 -0400

    dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
   def index
   	@tasks = current_user.tasks.all
   	user_id = current_user.id
-  	@proposals = Proposal.where("provider_id = #{user_id}")
+  	@proposals = Proposal.where(provider_id: user_id)
   end
 
 end
