022a42c76c3ce61cd790bd4ffcf6dc8f53abc050
Borja García de Vinuesa <borjavinuessa@gmail.com>
2014-05-04 08:16:28 -0400

    sd

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
   	@tasks = current_user.tasks.all
   	user_id = current_user.id
   	@proposals = Proposal.where(provider_id: user_id)
+  	
   end
 
 end
